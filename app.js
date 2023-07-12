d105edb4d16add9e0786598daef960a491fbcbc1
mateusb23 <mateusb.bispo@gmail.com>
2023-07-12 17:33:31 -0300

    adicionando um console.log()

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos de console")
+console.log("---> a Aplicação está conectando-se ao banco de dados")
+// mais um log de teste:
+console.log("---> Enviando para o GitHub")
